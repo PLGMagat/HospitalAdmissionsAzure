1971506fe075d4a49c72425ca36ebcbcced9b1ca
PLGMagat <95838339+PLGMagat@users.noreply.github.com>
2022-01-18 16:25:38 +0800

    Initial commit

---
+++ CREATE_TABLE - hospital_admission.sql
@@ -0,0 +1,12 @@
+CREATE TABLE hospital_admission(
+	Country VARCHAR(50)
+	, Indicator VARCHAR(50)
+	, [Date] DATE
+	, [Year_Week] VARCHAR(50)
+	, [Value] FLOAT
+	, [Source] VARCHAR(50)
+	, [URL] VARCHAR(256)
+)
+
+SELECT *
+FROM hospital_admission
